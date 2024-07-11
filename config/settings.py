66eefaa25b912c285e925db2b2c5e66a7bf5c376
MukhammadAli <rasulovadildora3@gmail.com>
2024-07-11 16:35:49 +0500

    changes in config for sdn

--- config/settings.py
+++ config/settings.py
@@ -131,12 +131,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'balita/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-MEDIA_URL = 'balita/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
