d9828ef8a8b4d8b2f800a23a487f3eca775ea919
MukhammadAli <rasulovadildora3@gmail.com>
2024-07-11 16:34:13 +0500

    changes in config for sdn

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from .views import home_view, blog_view, about_view, contact_view, blog_detail_v
 
 urlpatterns = [
     path('', home_view),
-    path('blog/', blog_view),
-    path('about/', about_view),
-    path('contact/', contact_view),
-    path('blog/<int:pk>/', blog_detail_view),
+    path('moose/blog/', blog_view),
+    path('moose/about/', about_view),
+    path('moose/contact/', contact_view),
+    path('moose/blog/<int:pk>/', blog_detail_view),
 ]
