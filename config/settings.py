1f547929ed2bf7154d4894a74df30136cfdca400
MukhammadAli <rasulovadildora3@gmail.com>
2024-08-17 18:46:46 +0500

    added ci cd

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&3_$hv4a@n$$r6zkj-c_z_9^6c!448^t@=w(dqn@dhe)*vd9l&
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
-# Application definition1
+# Application definition11
 
 INSTALLED_APPS = [
     'django.contrib.admin',
