712209d6ccfbc58cf2fc6e9068295c551062ff86
MukhammadAli <rasulovadildora3@gmail.com>
2024-09-04 12:35:27 +0500

    added ci cd

--- config/urls.py
+++ config/urls.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 urlpatterns = [
-    path('moose/admin/', admin.site.urls),
-    path('moose/', include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
     re_path(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
 ]
